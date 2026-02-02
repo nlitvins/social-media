2b7b819300660c0cc62e1104264e4bc12817519a
nlitvins <barbos3100@gmail.com>
2026-02-02 10:48:50 +0200

    SM-22: Error Handling

--- src/main/java/com/nlitvins/social_media/domain/graphqlexception/GlobalExceptionResolver.java
+++ src/main/java/com/nlitvins/social_media/domain/graphqlexception/GlobalExceptionResolver.java
@@ -5,19 +5,19 @@ import com.nlitvins.social_media.domain.exception.ErrorCode;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class GlobalExceptionResolver extends DataFetcherExceptionResolverAdapter {
 
     @Override
-    protected GraphQLError resolveToSingleError(
-            Throwable ex,
-            DataFetchingEnvironment env) {
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        log.error("Exception during GraphQL request: ", ex);
 
         if (ex instanceof BusinessException e) {
-
             return GraphqlErrorBuilder.newError(env)
                     .message(e.getErrorCode().getMessage())
                     .extensions(e.getDetails())
