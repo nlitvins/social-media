206c0c7cdb49cfe9ccf73def06ab0bbbc7291981
nlitvins <barbos3100@gmail.com>
2026-02-02 10:36:31 +0200

    SM-22: Error Handling

--- src/main/java/com/nlitvins/social_media/domain/graphqlexception/GlobalExceptionResolver.java
+++ src/main/java/com/nlitvins/social_media/domain/graphqlexception/GlobalExceptionResolver.java
@@ -2,16 +2,11 @@ package com.nlitvins.social_media.domain.graphqlexception;
 
 import com.nlitvins.social_media.domain.exception.BusinessException;
 import com.nlitvins.social_media.domain.exception.ErrorCode;
-import com.nlitvins.social_media.domain.exception.PostNotFoundException;
-import graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-import java.util.Map;
 
 @Component
 public class GlobalExceptionResolver extends DataFetcherExceptionResolverAdapter {
